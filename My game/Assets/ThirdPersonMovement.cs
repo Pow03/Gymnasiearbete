b0b9f86d66fe67c8df6e8138a5ed394563607f6d
Pow03 <03rasaxe03@gmail.com>
2022-02-03 17:26:57 +0100

    Version 3
    Added gravity and jumping

--- My game/Assets/ThirdPersonMovement.cs
+++ My game/Assets/ThirdPersonMovement.cs
@@ -9,11 +9,44 @@ public class ThirdPersonMovement : MonoBehaviour
 
     public float speed = 6f;
 
+    public float verticalVelocity;
+    public float gravity = 200.0f;
+    public float jumpForce = 10.0f;
+
     public float turnSmoothTime = 0.1f;
     float turnSmoothVelocity;
 
-    // Update is called once per frame
-    void Update()
+    #region - Awake - 
+
+    private void Start()
+    {
+        
+    }
+
+    #endregion
+
+    #region - Gravity -
+    private void Gravity()
+    {
+        if (controller.isGrounded)
+        {
+            verticalVelocity = -gravity * Time.deltaTime;
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                verticalVelocity = jumpForce;
+            }
+        }
+        else
+        {
+            verticalVelocity -= gravity * Time.deltaTime;
+        }
+        Vector3 moveVector = new Vector3(0, verticalVelocity, 0);
+        controller.Move(moveVector * Time.deltaTime);
+    }
+    #endregion
+
+    #region - Movement - 
+    private void Movement()
     {
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
@@ -29,4 +62,14 @@ public class ThirdPersonMovement : MonoBehaviour
             controller.Move(moveDir.normalized * speed * Time.deltaTime);
         }
     }
+    #endregion
+
+    #region - Update -
+    // Update is called once per frame
+    private void Update()
+    {
+        Movement();
+        Gravity();
+    }
+    #endregion
 }
